5d49e05b54f370c899e761f2d79c85b0290affa7
matzoese <98101352+matzoese@users.noreply.github.com>
2023-11-15 17:24:45 +0100

    final changes before the meeting, logging sadly still not working

--- HTML/Scripts/logging.js
+++ HTML/Scripts/logging.js
@@ -1,4 +1,22 @@
-//Define logging parameters as a class
+//Define logging parameters 
+CTATConfiguration.set("SessionLog", "true");
+CTATConfiguration.set("Logging", "ClientToLogServer");
+CTATConfiguration.set("dataset_name", "LoggingData");
+CTATConfiguration.set("dataset_level_type1", "pool");
+CTATConfiguration.set("dataset_level_type2", "task");
+CTATConfiguration.set("user_guid", "student1");
+CTATConfiguration.set("log_service_url","http://127.0.0.1:20080")
+/*
+CTATConfiguration.set("school_name", "CMU");
+CTATConfiguration.set("instructor_name", "A Teacher Name");
+CTATConfiguration.set("class_name", "22-512");
+*/
+
+function startLogging(){
+  var loggingLibrary=new CTATLoggingLibrary (true);
+  useDebugging=true;
+	loggingLibrary.start ();
+}
 
 class LoggingParameters {
   constructor(sessionLog, logging, datasetName, datasetLevelName1, datasetLevelType1, problemName, userGuid, sessionId, logServiceUrl) {
