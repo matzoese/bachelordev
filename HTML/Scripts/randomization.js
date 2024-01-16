c77b38f3a3da83d182235c5ebda45d4661ef546b
matzoese <98101352+matzoese@users.noreply.github.com>
2024-01-17 00:17:06 +0100

    Add files via upload

--- HTML/Scripts/randomization.js
+++ HTML/Scripts/randomization.js
@@ -1,19 +1,18 @@
-const taskPools = [];
-const tasks = [];
-
-taskPoolAmount = 1; //Amount of task pools with different combinations of class and topic
+/*taskPoolAmount = 1; //Amount of task pools with different combinations of class and topic
 taskAmount = 1; //Amount of tasks per task pool
+const tasks = [];
+const taskPools = [];
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max)
 }
 
 //This function returns an array of task pool numbers in a random order
 function randomizeTaskPools(){
     while (taskPools.length<taskPoolAmount){
-        generateNewTaskPool(taskPoolAmount);    
+        generateNewTaskPool(taskPoolAmount);
     }
-    return taskPools;
+    return taskPools
 }
 
 //This function returns a random number for a task pool that has not been generated yet for taskPools
@@ -21,7 +20,7 @@ function generateNewTaskPool(){
     let x = getRandomInt(taskPoolAmount);
     for(let i=0; i<taskPools.length;i++){
         if (taskPools[i]==x){
-            return;
+            return
         }
     }
     taskPools.push(x);
@@ -32,7 +31,7 @@ function randomizeTasks(){
     for(let i = 0; i<taskPools.length;i++){
         tasks[i]=getRandomInt(taskAmount);
     }
-    return tasks;
+    return tasks
 }
 
 //This function returns an array of arrays consisting of task pool number and task number
@@ -41,17 +40,77 @@ function generateTaskBatch(){
     randomizeTasks();
     const taskBatch = [];
     for(let i = 0; i<taskPools.length;i++){
-        taskBatch.push([taskPools[i],tasks[i]])
+        taskBatch.push([taskPools[i],tasks[i]]);
     }
-    return taskBatch;
+    return taskBatch
 }
 
 
 function loadTask([a,b]){
     let taskLink = 'pool'+a+'Task'+b;
-    return taskLink;
+    return taskLink
 }
 
 
 //Testing outputs
-//console.log(generateTaskBatch());
+console.log(generateTaskBatch());
+*/
+
+//This one is trying to generate the exact task names directly, much work for not a lot of reward tbh.
+const topics = ['L','V'];
+const difficulties = 1;
+const taskPools = [];
+const randomizedTasks = [];
+const taskAmount = topics.length*difficulties;
+const tasksPerPool = 2; //Amount of tasks per task pool
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max)
+}
+
+//Generate all combinations of topics and difficulties and store in taskPools
+
+function generateTaskPools(){
+    for (i = 0; i<topics.length; i++){
+        for (j = 0; j<difficulties;j++){
+            taskPools.push([topics[i],'C'+(j+1)])
+        }
+    }
+}
+generateTaskPools();
+
+//This function returns a random task pool which has not been generated yet (e.g [V, C0])
+function generateRandomizedTaskPool(){
+    let x = getRandomInt(taskAmount)
+    for(let i=0; i<randomizedTasks.length;i++){
+        if (randomizedTasks[i]==taskPools[x]){
+            return
+        }
+    }
+    randomizedTasks.push(taskPools[x]);
+}
+
+//Generates the task batch for the user
+function generateTaskBatch(){
+
+    while(randomizedTasks.length<taskAmount)
+        generateRandomizedTaskPool();
+
+    //adds specific task number for each task pool
+    for(i=0; i<taskAmount;i++){
+        x = getRandomInt(tasksPerPool);
+        randomizedTasks[i][0]=randomizedTasks[i][0]+(x+1);
+    }
+    
+    return randomizedTasks
+}
+
+function loadTask([a,b]){
+    let taskLink = a+b;
+    return taskLink
+}
+
+//Testing
+/*generateTaskBatch();
+console.log(randomizedTasks);
+*/
